9d3fded08aea53637126d70e5135f8a33ffad3aa
Hosea <karanja.mungai23@gmail.com>
2024-12-02 19:09:08 +0300

    passed tests

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode ="w") as write_file:
+        write_file.write(file_content)        
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a") as append_file:
+        append_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt") as read_me:
+        return read_me.read()
